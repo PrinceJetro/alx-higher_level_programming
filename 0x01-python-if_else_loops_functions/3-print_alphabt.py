e916bd7eb5fe12b5ff6d22bc7d428fb85b2ff27c
Prince Jetro <adegbuyijephthah@gmail.com>
2023-03-07 03:54:28 -0800

    errors

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97,123):
-    if chr(i) != 'e' and chr(i) != "q":
-        print(f"{chr(i)}")
+for char in range(26):
+    if char != 4 and char != 16:
+        print("{:s}".format(chr(char + ord("a"))), end="")
